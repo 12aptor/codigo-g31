b20c4ebaa4d01ea2304bfd02c9d67fb95a1fe2d1
12aptor <paholo2054@gmail.com>
2026-05-13 21:40:11 -0500

    Creación del repositorio Codigo g31

---
+++ semana01/clase01/02_sintaxis.py
@@ -0,0 +1,8 @@
+def suma(a, b):
+    return a + b
+
+numero = 20
+
+if numero > 10:
+    resultado = suma(5, 2)
+    print(resultado)
---
+++ semana01/clase01/03_tipos_de_datos.py
@@ -0,0 +1,14 @@
+print(type(10))
+print(type("Hola mundo"))
+print(type(1.05))
+print(type(False))
+print(type(True))
+print(type(None))
+
+print(float("5.5"))
+print(bool("Hola"))
+print(bool(""))
+print(bool(" "))
+
+if 10 > 5:
+    print("Ingresó al if")
