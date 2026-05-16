e964470165f6b9d1627bc38406ddd584541d6c78
12aptor <paholo2054@gmail.com>
2026-05-15 22:45:20 -0500

    bucles

---
+++ semana01/clase02/04_bucles.py
@@ -0,0 +1,12 @@
+animales = ["😺", "🐶", "🐷", "🐮", "🐍"]
+for animal in animales:
+    print(animal)
+
+
+caracteres = "Hola mundo"
+for c in caracteres:
+    print(c)
+
+
+for i, animal in enumerate(animales):
+    print(i, animal)
