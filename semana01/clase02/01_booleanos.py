96f50f31403b37e1838ba895bef7a45d1422c1bf
12aptor <paholo2054@gmail.com>
2026-05-15 22:33:16 -0500

    listas

---
+++ semana01/clase02/01_booleanos.py
@@ -0,0 +1,14 @@
+print(True)
+print(False)
+
+print(5 > 3)
+print(3 > 5)
+
+print("m" > "p")
+print("p" > "m")
+
+print("Hola" == "hola")
+
+print(True and False)
+print(True or False)
+print(not True)
