46c2b59aabb098be77705b2ad3eef42676429041
12aptor <paholo2054@gmail.com>
2026-06-12 22:46:16 -0500

    Flask con Sqlalchemy

---
+++ semana05/flask-sqlalchemy/README.md
@@ -0,0 +1,19 @@
+# Flask Sqlalchemy
+
+## Crear el proyecto
+
+```bash
+python -m venv nombre_del_entorno
+source nombre_del_entorno/Scripts/activate
+source nombre_del_entorno/bin/activate
+nombre_del_entorno\Scripts\activate
+```
+
+## Instalar dependencias
+```bash
+pip install Flask Flask-sqlalchemy python-dotenv psycopg2-binary
+```
+
+## Ejecutar las migraciones
+
+## Ejecutar el proyecto
---
+++ semana05/flask-sqlalchemy/app.py
@@ -0,0 +1,67 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+from sqlalchemy import DateTime, Text, func, select
+from sqlalchemy.orm import Mapped, mapped_column, Session
+from datetime import datetime
+
+load_dotenv()
+
+db_user = os.getenv('DB_USER')
+db_password = os.getenv('DB_PASSWORD')
+db_host = os.getenv('DB_HOST')
+db_port = os.getenv('DB_PORT')
+db_name = os.getenv('DB_NAME')
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}'
+db = SQLAlchemy()
+db.init_app(app)
+
+class User(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    email: Mapped[str] = mapped_column(unique=True)
+    password: Mapped[str] = mapped_column(Text)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=func.now())
+
+with app.app_context():
+    db.create_all()
+
+@app.route("/")
+def home():
+    return 'Hello from Flask 🖐️'
+
+@app.route("/users", methods=['GET', 'POST'])
+def users():
+    method = request.method
+
+    if method == 'POST':
+        data = request.get_json()
+        user = User(
+            name=data['name'],
+            email=data['email'],
+            password=data['password']
+        )
+        db.session.add(user)
+        db.session.commit()
+
+        return jsonify({
+            'message': 'User created successfully'
+        }), 200
+    elif method == 'GET':
+        users = User.query.all()
+        
+        formated_users = []
+        for user in users:
+            formated_users.append({
+                'id': user.id,
+                'name': user.name,
+                'email': user.email,
+                'created_at': str(user.created_at)
+            })
+        return jsonify(formated_users), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
