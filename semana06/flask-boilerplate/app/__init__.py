8424b7305ca5a00025e35d313a72d4dd3f703007
12aptor <paholo2054@gmail.com>
2026-08-12 20:27:50 -0500

    Flask Boilerplate: Configuracion de cors

--- semana06/flask-boilerplate/app/__init__.py
+++ semana06/flask-boilerplate/app/__init__.py
@@ -3,12 +3,14 @@ from db import db
 from config import Config
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db.init_app(app)
 migrate = Migrate(app, db)
 jwt = JWTManager(app)
+cors = CORS(app)
 
 from app.models import (
     category_model,
